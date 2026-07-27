d61656c01189ab4e6b74f80cb93002a9fedd8ff3
Saad Hameed <saadhameed588@gmail.com>
2026-07-27 16:17:55 +0500

    starting shadcn here

--- frontend/src/shared/components/Sidebar.tsx
+++ frontend/src/shared/components/Sidebar.tsx
@@ -45,7 +45,7 @@ export const Sidebar = ({ isOpen, setIsOpen }: SidebarProps) => {
           <div className="flex items-center gap-2">
             <TbArrowBadgeRightFilled
               className={`size-5 ${
-                active === "home" && highlight==true ? "visible" : "invisible"
+                active === "home" ? "visible" : "invisible"
               }`}
             />
 
@@ -54,7 +54,6 @@ export const Sidebar = ({ isOpen, setIsOpen }: SidebarProps) => {
             </Link>
           </div>
 
-          {/* Add more links here */}
         </div>
       </nav>
 
